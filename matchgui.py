85e0f7ac218af9b3c108b2a06ca2a759f9708eb9
human-who-codes <connorbrooks18@icloud.com>
2022-02-22 22:53:57 -0600

    Finished GUI for matches and highlighted matches

--- matchgui.py
+++ matchgui.py
@@ -1,10 +1,16 @@
 # create a GUI for the matchmaker
+
 # Use opponent_matches.py to get the opponent matches
+
 # Use user input to create the matchesabs
 
+
 import tkinter as tk
+
 from tkinter import ttk
+
 from tkinter import messagebox
+
 from tkinter import filedialog
 import opponent_matches
 import getmatches
@@ -14,52 +20,144 @@ import getmatches
 
 
 
-
+URL = "http://ftc-events.firstinspires.org/2021/USIACMPBLA/qualifications"
+print(URL)
 
 def main():
+
     root = init_window()
     add_components(root)
+
     root.mainloop()
 
 
+
 # init main window
+
 def init_window():
+
     root = tk.Tk()
+
     root.title('Matchmaker')
+
     root.geometry('800x800')
+
+    root.columnconfigure(0, weight=100)
+
+    root.columnconfigure(1, weight=100)
+
+    root.columnconfigure(2, weight=100)
+
+    root.columnconfigure(3, weight=100)
+
+    root.columnconfigure(4, weight=100)
+
+    root.columnconfigure(5, weight=10)
+    
+
+    # set the root's internal padding
+
+    root['padx'] = 20
+    
     return root
+
 
 def add_components(root):
     url_input = add_url_input(root)
-    get_matches_button = add_get_matches_button(root, url_input.get())
 
-    get_matches_button.pack(side="top")
-    url_input.pack(pady=10)
+    get_matches_button = add_get_matches_button(root, url_input)
+
+
+    get_matches_button.grid(column=4, columnspan=2, row=0, pady=5)
+
+    url_input.grid(column = 0, columnspan = 3, row=0, pady=5)
+
+    table = show_matches(root, url_input.get())
+
+    table.grid(column=0, columnspan=5, row=1, pady=5)
+
 
 # add a input for a url in the center top of the window
-# large font
+
 def add_url_input(root):
-    url_input = tk.Entry(root, width=50, justify="center", font=("Robot", 14))
+
+    url_input = tk.Entry(root, width=60, justify="center", font=("Robot", 14))
+
     # url_input.grid(row=0, column=0, columnspan=2)
+
     url_input.insert(0, 
-    'ftc-events.firstinspires.org/2021/USIACMPBLA/qualifications')
-    # check if url has http://
-    if 'http://' not in url_input.get():
-        url_input.insert(0, 'http://')
+
+    URL)
+    print(URL)
+
+
     return url_input
 
+
 # add a button to get the matches
+
 def add_get_matches_button(root, url_input):
-    get_matches_button = tk.Button(root, text='Get Matches', command=lambda: get_matches(url_input),
+
+    get_matches_button = tk.Button(root, text='Enter', command=lambda: refresh(root, url_input),
+
                     font=("Robot", 15))
+
     # get_matches_button.grid(row=1, column=2)
+
     return get_matches_button
 
-def get_matches(url_input):
-    all_matches = getmatches.get_matches(url_input)
+
+# refresh table
+def refresh(root, url_input_box):
+    url_input = url_input_box.get()
+    for child in root.winfo_children():
+        child.destroy()
+    global URL
+    URL = url_input
+    add_components(root)
+# interact with other files' functions 
+
+def get_important_matches(root, url_input):
+    all_matches = get_all_matches(url_input)
+
     matches = opponent_matches.get_important_matches(all_matches, team='8672')
-    print(matches)
-    
-main()
+    return matches
+
+def get_all_matches(url_input):
+
+    return getmatches.get_matches(url_input)
+
+
+# table full of matches using the functions ^---
+
+def show_matches(root, url_input):
+    all_matches = get_all_matches(url_input)
+    important_matches = get_important_matches(root, url_input)
+    textbox = tk.Text(root, width=70, height=20, font=("Robot", 14))
+    add_text_to_table(textbox, all_matches, important_matches)
+    #add a scrollbar
+    scrollbar = tk.Scrollbar(root, command=textbox.yview)
+    scrollbar.grid(row=1, column=5, sticky='nsew')
+    textbox['yscrollcommand'] = scrollbar.set
+    return textbox
+
+def add_text_to_table(textbox, all_matches, important_matches):
+    textbox.tag_configure('highlight', background='yellow')
+    for i in range(len(all_matches)):
+        for j in [0, 1, 2, 3, 4]:
+            string = str(all_matches[i][j]) + '\t'
+            if j == 4: string += "\n"
+            if i + 1 in important_matches:
+                # add tag to this line
+                textbox.insert(tk.END, string, 'highlight')
+            else: textbox.insert(tk.END, string)
+            # check to see if row number is in important matches
+
+# actually begin the window
+
+if __name__ == '__main__':
+    main()
+
+
 
 
